2058e5e88fc0f0b492e108865fce9fcda9a2bf8c
hellochopra1 <hellochopra1@gmail.com>
2016-05-31 15:56:22 +0530

    First Commit for code review
    First Commit for code review

---
+++ TCP/CommonClasses/GetAuthorizationCodeParams.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP.CommonClasses
+{
+    class GetAuthorizationCodeParams
+    {
+        [JsonProperty("oxd_id")]
+        public string _oxd_id;
+
+        [JsonProperty("username")]
+        private string _username;
+
+        [JsonProperty("password")] 
+        private string _password;
+
+        [JsonProperty("acr_values")]
+        private List<string> _setAcrValues;
+
+        [JsonIgnore]
+        [JsonProperty("testignore")]
+        private List<string> _testIgnore;
+
+        public void SetOxdId(string val)
+        {
+            this._oxd_id = val;
+        }
+
+        public void SetAcrValues(List<string> val)
+        {
+            this._setAcrValues = val;
+        }
+
+        public void SetUserName(string val)
+        {
+            this._username = val;
+        }
+
+        public void SetPassword(string val)
+        {
+            this._password = val;
+        }
+
+        public string GetOxdId()
+        {
+            return _oxd_id;
+        }
+
+        public List<string> GetAcrValues()
+        {
+            return _setAcrValues;
+        }
+
+        public string GetUserName()
+        {
+            return _username;
+        }
+
+        public string SetPassword()
+        {
+            return _password;
+        }
+    }
+}
---
+++ TCP/client/update_site_registration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCP.Classes;
+
+namespace TCP.client
+{
+    class update_site_registration
+    {
+        public string UpdateSiteRegisteration()
+        {
+            CommandClient client = new CommandClient("127.0.0.1", "8099");
+
+            UpdateSiteParams param = new UpdateSiteParams();
+            param.SetOxdId("14c7ed41-cb45-4bcc-b5c6-d311a44c8a91");
+            param.SetAuthorizationRedirectUri("http://www.omsttech.com/wp-login.php");
+            param.SetPostLogoutRedirectUri("http://www.omsttech.com/wp-login.php?action=logout&_wpnonce=a3c70643e9");
+            param.SetApplicationType("web");
+            param.SetRedirectUris(Lists.newArrayList(new string[] { "http://www.omsttech.com/wp-login.php" }));
+            param.SetAcrValues(new List<string>());
+            param.SetClientJwksUri("");
+            param.SetContacts(Lists.newArrayList(new string[] { "hellochopra1@gmail.com" }));
+            param.SetGrantType(Lists.newArrayList(new string[] { "authorization_code" }));
+            param.SetClientTokenEndpointAuthMethod("");
+            param.SetClientLogoutUri(Lists.newArrayList(new string[] { "http://www.omsttech.com/wp-login.php?action=logout&_wpnonce=a3c70643e9" }));
+
+            //param.Op_host("https://ce-dev2.gluu.org");
+
+            Command cmd = new Command(CommandType.update_site_registration);
+            cmd.setParamsObject(param);
+            return client.send(cmd);
+        }
+    }
+}
