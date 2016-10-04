e2490e68a8db6da5cafde12150095a9c3f12a1f0
Vasanthakumar V <adengappaa@hotmail.com>
2016-10-04 23:20:58 +0530

    Included the demo web app for oxd server.

---
+++ GluuDemoWebsite/Controllers/HomeController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GluuDemoWebsite.Models;
+using CSharp.CommonClasses;
+using oxdCSharp.CommandResponses;
+using oxdCSharp.Clients;
+using oxdCSharp.CommandParameters;
+
+namespace GluuDemoWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(OxdModel oxd)
+        {
+            var registerSiteInputParams = new RegisterSiteParams();
+            var registerSiteClient = new RegisterSiteClient();
+
+            //prepare input params for Register Site
+            registerSiteInputParams.AuthorizationRedirectUri = oxd.RedirectUrl;
+            registerSiteInputParams.OpHost = "https://scim-test.gluu.org";
+            registerSiteInputParams.ClientName = "VasOxdTestingClient-CanBeRemoved";
+            
+            //Register Site
+            var registerSiteResponse = registerSiteClient.RegisterSite(oxd.OxdHost, oxd.OxdPort, registerSiteInputParams);
+
+            //Process the response
+            return Json(new { oxdId = registerSiteResponse.Data.OxdId });
+        }
+
+        [HttpPost]
+        public ActionResult Update(OxdModel oxd)
+        {
+            var updateSiteInputParams = new UpdateSiteParams();
+            var updateSiteClient = new UpdateSiteRegistrationClient();
+
+            //prepare input params for Update Site Registration
+            updateSiteInputParams.OxdId = oxd.OxdId;
+            updateSiteInputParams.Contacts = new List<string> { oxd.OxdEmail };
+            updateSiteInputParams.PostLogoutRedirectUri = oxd.PostLogoutRedirectUrl;
+
+            //Update Site Registration
+            var updateSiteResponse = updateSiteClient.UpdateSiteRegistration(oxd.OxdHost, oxd.OxdPort, updateSiteInputParams);
+
+            //Process the response
+            return Json(new { status = updateSiteResponse.Status });
+        }
+
+        [HttpPost]
+        public ActionResult AuthUrl(OxdModel oxd)
+        {
+            var getAuthUrlInputParams = new GetAuthorizationUrlParams();
+            var getAuthUrlClient = new GetAuthorizationUrlClient();
+
+            //prepare input params for Getting Auth URL from a site
+            getAuthUrlInputParams.OxdId = oxd.OxdId;
+
+            //Get Auth URL
+            var getAuthUrlResponse = getAuthUrlClient.GetAuthorizationURL(oxd.OxdHost, oxd.OxdPort, getAuthUrlInputParams);
+
+            //Process Response
+            return Json(new { authUrl = getAuthUrlResponse.Data.AuthorizationUrl });
+        }
+
+        [HttpPost]
+        public ActionResult GetTokens(OxdModel oxd)
+        {
+            var getTokenByCodeInputParams = new GetTokensByCodeParams();
+            var getTokenByCodeClient = new GetTokensByCodeClient();
+
+            //prepare input params for Getting Tokens from a site
+            getTokenByCodeInputParams.OxdId = oxd.OxdId;
+            getTokenByCodeInputParams.Code = oxd.AuthCode;
+            getTokenByCodeInputParams.State = oxd.AuthState;
+
+            //Get Tokens by Code
+            var getTokensByCodeResponse = getTokenByCodeClient.GetTokensByCode(oxd.OxdHost, oxd.OxdPort, getTokenByCodeInputParams);
+
+            //Process response
+            return Json(new { accessToken = getTokensByCodeResponse.Data.AccessToken, refreshToken = getTokensByCodeResponse.Data.RefreshToken });
+        }
+
+        [HttpPost]
+        public ActionResult GetUserInfo(OxdModel oxd)
+        {
+            var getUserInfoInputParams = new GetUserInfoParams();
+            var getUserInfoClient = new GetUserInfoClient();
+
+            //prepare input params for Getting User Info from a site
+            getUserInfoInputParams.OxdId = oxd.OxdId;
+            getUserInfoInputParams.AccessToken = oxd.AccessToken;
+
+            //Get User Info
+            var getUserInfoResponse = getUserInfoClient.GetUserInfo(oxd.OxdHost, oxd.OxdPort, getUserInfoInputParams);
+
+            //Process response
+            var userName = getUserInfoResponse.Data.UserClaims.Name.First();
+            var userEmail = getUserInfoResponse.Data.UserClaims.Email == null ? string.Empty : getUserInfoResponse.Data.UserClaims.Email.FirstOrDefault();
+
+            return Json(new { userName = userName });
+        }
+
+        [HttpPost]
+        public ActionResult GetLogoutUri(OxdModel oxd)
+        {
+            var getLogoutUriInputParams = new GetLogoutUrlParams();
+            var getLogoutUriClient = new GetLogoutUriClient();
+
+            //prepare input params for Getting Logout URI from a site
+            getLogoutUriInputParams.OxdId = oxd.OxdId;
+
+            //Get Logout URI
+            var getLogoutUriResponse = getLogoutUriClient.GetLogoutURL(oxd.OxdHost, oxd.OxdPort, getLogoutUriInputParams);
+
+            //Process response
+            return Json(new { logoutUri = getLogoutUriResponse.Data.LogoutUri });
+        }
+
+        public ActionResult CallBack(string code, string state, string scope)
+        {
+            return Json(new { authCode = code, authState = state, scope = scope }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GluuDemoWebsite/Models/OxdModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GluuDemoWebsite.Models
+{
+    public class OxdModel
+    {
+        [JsonProperty("oxdHost")]
+        public string OxdHost { get; set; }
+
+        [JsonProperty("oxdPort")]
+        public int OxdPort { get; set; }
+
+        [JsonProperty("redirectUrl")]
+        public string RedirectUrl { get; set; }        
+
+        [JsonProperty("oxdId")]
+        public string OxdId { get; set; }
+
+        [JsonProperty("postLogoutRedirectUrl")]
+        public string PostLogoutRedirectUrl { get; set; }
+
+        [JsonProperty("oxdEmail")]
+        public string OxdEmail { get; set; }
+
+        [JsonProperty("authUrl")]
+        public string AuthUrl { get; set; }
+
+        [JsonProperty("authCode")]
+        public string AuthCode { get; set; }
+
+        [JsonProperty("authState")]
+        public string AuthState { get; set; }
+
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("refreshToken")]
+        public string RefreshToken { get; set; }
+    }
+}
